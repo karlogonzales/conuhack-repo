429421e7dae310ed314e262cab9620a89248f005
Marc Yang <m.y.yang001@gmail.com>
2017-01-21 17:18:27 -0500

    integrated with clarifai api, now it is returning a name token

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 var express	=	require("express");
 var multer	=	require('multer');
 var fs = require('fs');
+
 var app	=	express();
 
+var Clarifai = require('clarifai');
+var clarifai_app = new Clarifai.App(
+    'CrJS4jQSyV6qWixZuShUq8zLOwrIIz2yFDex87jV',
+    '5QxnJDzgJlNlp0DgTP68oVaZ1SGRkipN9AT4OKjV'
+  );
 
-var storage = multer.memoryStorage()
+
+
+
+var storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './uploads');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.originalname);
+  }
+});
 var upload = multer({ storage: storage }).single("userPhoto");
 
 
@@ -14,20 +30,71 @@ app.get('/',function(req,res){
 
 app.post('/api/photo',function(req,res){
 	
+
 	upload(req,res,function(err) {
 		if(err) {
+
 			return res.end("Error uploading file.");
 		}
-		console.log('Should be the buffer:', req.file.buffer)
+		console.log('file location and name: ', req.file.destination +'/' +req.file.filename);
 		
-		// send the buffer to claifai api to recognize
+		var filePath = req.file.destination +'/' +req.file.filename;
+		// send the buffer to claifai api to recognize, return the token
+
+		fs.readFile(filePath, function(err, original_data){
+    		var base64Image = original_data.toString('base64');
+			//console.log(base64Image);
+			// predict the contents of an image by passing in a url
+			//var modelid = fad6d3c22c9b4b4eaac676da31324e4e;
+			console.log(Clarifai.TRAVEL_MODEL);
+			//   clarifai_app.models.get("church").then(
+    		// 	function(response) {
+			// 		console.log(response);
+    		// 	},
+    		// 	function(err) {
+      		// 		// there was an error
+			// 		  console.log(err);
+    		// 		});
+			
+			clarifai_app.models.predict("church", {base64: base64Image}).then(
+				function(response) {
+					var itemName = response.data.outputs[0].data.concepts[0];
+				console.log();
+				/*
+					{ id: 'florence cathedral',
+					name: 'florence cathedral',
+					app_id: 'b84c6efb058441fca1c27d8e8df429a8',
+					value: 0.3502867 }
+
+				*/
+
+				//fetch (itemName.name callback()) // fetch wiki contents
+				
+
+
+			},
+			function(err) {
+				console.error(err);
+			});	
+    
+		});
+
+
 		
-
-
-
+		
+		
+		// delete the file from the middleware
+		//fs.unlinkSync(filePath);
+		
 		res.end("File is uploaded");
 	});
 });
+
+// serves static files 
+app.get(/^(.+)$/, function(req, res){ 
+     res.sendFile( __dirname +"/uploads/"+ req.params[0]); 
+});
+
 
 app.listen(3000,function(){
     console.log("Working on port 3000");
