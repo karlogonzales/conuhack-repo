e18b925ecf7f7bc6fed908398aa83ba606228ed2
Tedderic <ted_ruby@hotmail.com>
2017-01-22 02:50:12 -0500

    Attempt to add a thread

---
+++ runVoice.js
@@ -0,0 +1,9 @@
+VoiceRSS.speech({
+              key: 'b49727a235b54b15a7b22955f9fbd2d6',
+              src: document.getElementById('status').innerHTML,
+              hl: 'en-us',
+              r: 0, 
+              c: 'mp3',
+              f: '44khz_16bit_stereo',
+              ssml: false
+          });
